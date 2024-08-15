edb8505f638f306c5692fd646ebea84fdb6908bb
jbiernaciak <jbiernaciak@gmail.com>
2024-08-15 23:36:42 +0200

    Time: 330 ms (39.88%) | Memory: 57.6 MB (43.56%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public bool LemonadeChange(int[] bills) {
+        var (numberOf5, numberOf10) = (0, 0);
+        foreach(var bill in bills) {
+            (numberOf5, numberOf10) = bill switch {
+                5 => (numberOf5 + 1, numberOf10),
+                10 => (numberOf5 - 1, numberOf10 + 1),
+                20 when numberOf10 == 0 => (numberOf5 - 3, 0),
+                _ => (numberOf5 - 1, numberOf10 - 1)
+            };
+
+            if (numberOf5 < 0 || numberOf10 < 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
